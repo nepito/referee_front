7ccbaf203182d22fd942ed2e3ecbaac5419c90c1
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-04-29 19:19:03 -0700

    🚧 Add players

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,31 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 event, fix_data = st.tabs(["Eventos", "Curación de datos"])
 
+matches = {
+    "Eco Pasto vs Vaqueros": ["Eco Pasto", "Vaqueros FC"],
+    "Backpack Boyz vs Kingstone": ["Backpack Boyz", "Kingstone"],
+    "Troncos vs Mesebrios": ["Troncos FC", "Mesebrios"]
+}
+jugadores = pd.read_csv("/workdir/tests/data/jugadores.csv")
 team_list: list = ["Equipo A", "Equipo B"]
+def get_players_from_match(fixture):
+    teams = matches[fixture]
+    return {
+        teams[0]: list(jugadores[jugadores.equipo == teams[0]].jugador),
+        teams[1]: list(jugadores[jugadores.equipo == teams[1]].jugador)
+        }
+
 players = {
     "Equipo A": ["Hector", "Puma", "Nepo"],
     "Equipo B": ["Peso Pluma", "Mirra", "Naim"]
 }
 
 with event:
+    fixture = st.selectbox("Selecciona el partido:", list(matches.keys()))
+    players = get_players_from_match(fixture)
     if 'events' not in st.session_state:
         st.session_state.events = []
+    team_list = list(players.keys())
     team = st.selectbox("Selecciona al equipo:", team_list)
     player_list = players[team]
     player = st.selectbox("Selecciona al anotador:", player_list)
